db47276cd6bffe061c45e010cc681dbe96fc0630
VCleitoso <90481485+VCleitoso@users.noreply.github.com>
2021-11-24 22:29:13 -0300

    Update AlgoritmoTeste - Copia.c

--- AlgoritmoTeste - Copia.c
+++ AlgoritmoTeste - Copia.c
@@ -1,49 +1,56 @@
 /* Programa para PAC II:
- * Monitor o volume na sua caixa d'·Ågua
- * Biblioteca da Placa: "esp8266 by ESP8266 Community vers„o 2.3.0"
+ * Monitor o volume na sua caixa d'√°¬Ågua
+ * Biblioteca da Placa: "esp8266 by ESP8266 Community vers√£o 2.3.0"
  * Placa: "NodeMCU 1.0 (ESP-12E Module)"
  * Upload Speed: "115200"
  * CPU Frequency: "160MHz"
  * //https://dl.espressif.com/dl/package_esp32_index.json
 */
 //=====================================================================
-// --- Inclus„o de bibliotecas ---
+// --- Inclus√£o de bibliotecas ---
 #include <WiFi.h> 
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
+#include "SoftwareSerial.h"
+//Tem que incluir biblioteca para testes
 // ======================================================================
 // --- Dados de Acesso do seu roteador ---
-#define WLAN_SSID       "12345678" // InformaÁ„o da SSID do seu roteador
+#define WLAN_SSID       "12345678" // Informa√ß√£o da SSID do seu roteador
 #define WLAN_PASS       "12345678"   // senha de acesso do seu roteador
 // ======================================================================
 // --- Dados de Acesso da Plataforma Adafruit IO ---
 #define AIO_SERVER      "io.adafruit.com"     // manter fixo
 #define AIO_SERVERPORT  1883                  // manter fixo
-#define AIO_USERNAME    "marcelomoresco"        // sua informaÁ„o
-#define AIO_KEY         "projeto-pac" // sua informaÁ„o
+#define AIO_USERNAME    "marcelomoresco"        // sua informa√ß√£o
+#define AIO_KEY         "projeto-pac" // sua informa√ß√£o
 // ======================================================================
 // --- Mapeamento de Hardware ---
 #define trigPin 22  //D1 - PINO DE TRIGGER PARA SENSOR ULTRASSONICO
 #define echoPin 23  //D2 - PINO DE ECHO PARA SENSOR ULTRASSONICO
  
-WiFiClient client; // cria uma classe WiFiClient com o ESP8266 para conex„o com o servidor MQTT
+WiFiClient client; // cria uma classe WiFiClient com o ESP8266 para conex√£o com o servidor MQTT
  
 // Configura a classe MQTT passando para o WiFi cliente e Servidor MQTT os detalhes do login
 Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
  
-// O endereÁamento para enviar os arquivos seguem o formato: <username>/feeds/<feedname>
+// O endere√ßamento para enviar os arquivos seguem o formato: <username>/feeds/<feedname>
 Adafruit_MQTT_Publish volume = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/volume");
+// ======================================================================
+// --- Confirgura√ß√£o Slave
+SoftwareSerial blackBoardMaster(40,41); // (RX, TX)
+
+// ======================================================================
  
 // ======================================================================
-// --- Vari·veis Globais ---
+// --- Vari√°veis Globais ---
 long duracao = 0;
 float distancia = 0;
 
 // ======================================================================
 // --- Void Setup ---
 void setup() {
-   
-  Serial.begin(9600); // inicia comunicaÁ„o serial com velocidade 9600
+  blackBoardMaster.begin(9600);
+  Serial.begin(9600); // inicia comunica√ß√£o serial com velocidade 9600
 
    
   Serial.println(F("Monitorar Volume Caixa D'agua - Adafruit MQTT")); // escreve na serial   
@@ -60,19 +67,19 @@ void setup() {
   Serial.println();
  
   Serial.println("WiFi Conectado");
-  Serial.println("IP endere√ßo: "); Serial.println(WiFi.localIP());
+  Serial.println("IP endere√É¬ßo: "); Serial.println(WiFi.localIP());
    
 // ======================================================================
-// --- ConfiguraÁ„o IO ---
-  pinMode(trigPin, OUTPUT); // pino D1 como saÌda para TRIGGER
+// --- Configura√ß√£o IO ---
+  pinMode(trigPin, OUTPUT); // pino D1 como sa√≠da para TRIGGER
   pinMode(echoPin, INPUT);  // pino D2 como entrada para ECHO
 }
 // ======================================================================
 // --- void loop ---
 void loop() {
-  MQTT_connect();   // chama funÁ„o para conex„o com MQTT server
+  MQTT_connect();   // chama fun√ß√£o para conex√£o com MQTT server
    
-  digitalWrite(trigPin, LOW);    // Limpa trigPin
+  /*digitalWrite(trigPin, LOW);    // Limpa trigPin
   delayMicroseconds(2);          // aguarda 2 microsegundos
  
   digitalWrite(trigPin, HIGH);   // Seta trigPin HIGH aguarda 
@@ -81,49 +88,51 @@ void loop() {
    
   // Leitura do echoPin, retorna a onda de som em microsegundos
   duracao = pulseIn(echoPin, HIGH);
+  Vai receber vari√°vel distancia do arduino uno*/
+  char duracao = blackBoardMaster.read(); //recebe dura√ß√£o do arduino uno
   distancia= duracao*0.034/2;
   distancia = distancia*10; // converter cm para mm
  
-  if (distancia > 96){  // leitura minima. ReservatÛrio vazio
+  if (distancia > 96){  // leitura minima. Reservat√≥rio vazio
     distancia = 96;
   }
-    if (distancia < 37){  // leitura m·xima. ReservatÛrio vazio
+    if (distancia < 37){  // leitura m√°xima. Reservat√≥rio vazio
     distancia = 37;
   }
  int volumevar = map(distancia, 37, 96, 1000, 0); 
   /* Remapeia o range de leitura
-   * Ao invÈs de ler de 37 a 96, ler· de 1000 a 0*/
+   * Ao inv√©s de ler de 37 a 96, ler√° de 1000 a 0*/
  Serial.print("distancia:"); // imprime "distancia:"
   Serial.println(distancia);  // imprime a variavel distancia
   Serial.print("volume:");    // imprime "volume:"
   Serial.println(volumevar);  // imprime a variavel volume
    
   volume.publish(volumevar);     // publica variavel "distancia" em no feed "volume"
-  // nossa saÌda ser· em mL
+  // nossa sa√≠da ser√° em mL
    
  
    
   delay(3000); // aguarda 3 segundos
-  /* ObservaÁ„o: A plataforma Adafruit IO sÛ permite 30 publicaÁıes/minuto
-   * na conta gratuita. Ent„o È importante n„o ultrapassar ou sua leitura 
-   * na dashboard ser· inst·vel e incorreta.*/
+  /* Observa√ß√£o: A plataforma Adafruit IO s√≥ permite 30 publica√ß√µes/minuto
+   * na conta gratuita. Ent√£o √© importante n√£o ultrapassar ou sua leitura 
+   * na dashboard ser√° inst√°vel e incorreta.*/
 }
  
 // ======================================================================
-// --- FunÁ„o para Conex„o / Reconex„o MQTT ---
+// --- Fun√ß√£o para Conex√£o / Reconex√£o MQTT ---
   void MQTT_connect() {
     int8_t ret;
    
-    // para de tentar reconex„o quando consegue
+    // para de tentar reconex√£o quando consegue
     if (mqtt.connected()) {
       return;
     }
     Serial.print("Conectando ao MQTT... "); // imprime na serial
-    // tenta conex„o 5 vezes. Depois WatchDogTime!
+    // tenta conex√£o 5 vezes. Depois WatchDogTime!
     uint8_t retries = 5;
-    while ((ret = mqtt.connect()) != 0) { // conectar· quando retornar 0
+    while ((ret = mqtt.connect()) != 0) { // conectar√° quando retornar 0
       Serial.println(mqtt.connectErrorString(ret));
-      Serial.println("Nova tentativa de conex√£o MQTT em 5 segundos...");
+      Serial.println("Nova tentativa de conex√É¬£o MQTT em 5 segundos...");
       mqtt.disconnect();
       delay(5000);  // aguarda 5 segundos
       retries--;
