2d68876d34cb13d593fa785eea481d0b27642c21
VCleitoso <90481485+VCleitoso@users.noreply.github.com>
2021-11-25 20:10:48 -0300

    Update Sensor Arduino Uno.c

--- Sensor Arduino Uno.c
+++ Sensor Arduino Uno.c
@@ -2,10 +2,11 @@
 #define echoPin A1
 #include "SoftwareSerial.h"
 
-SoftwareSerial blackBoardSlave(A4,A5); // (RX, TX), Faz comunicação master com o esp, usar portas certas
-/* Usei software seial, pois achei mais fácil de entender.*/
+int duration;
 
-long duration;
+SoftwareSerial blackBoardSlave(A4,A5); 
+/* Usei software serial, pois achei mais fácil de entender.*/
+
 
 void setup(){
   blackBoardSlave.begin(9600);
@@ -17,8 +18,8 @@ void setup(){
 }
 
 void loop() {
-  
- void duration(trigPin,echoPin);
+ 
+duracao();
 
  blackBoardSlave.print(duration); //Envia variável duration pro esp
 
@@ -29,7 +30,7 @@ void loop() {
 }
 
 //Função para mandar a variável duration
-void duration(trigPin,echoPin){
+void duracao(){
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   // Sets the trigPin on HIGH state for 10 micro seconds
@@ -37,5 +38,5 @@ void duration(trigPin,echoPin){
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);   
-  return duration;
+  long duration;
 }
